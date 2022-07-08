92278a29d33e578c6ef72c295fe347c92549f017
ALICE LIANG <81024991+Art-liang@users.noreply.github.com>
2022-07-08 22:40:20 +0800

    Array

---
+++ index2.js
@@ -0,0 +1,33 @@
+// let classA =['名字A','名字B']陣列用大括號,在裡面打名字
+// console.log('轉學前:',classA)
+//  陣列基本上沒有長度的限制
+
+// classA.push('小黃') 假設班級來一個轉學生,這段就是可以在後面直接新增
+// console.log('轉學後:', classA)
+
+
+
+// 物件 Array
+let classA = ['小明', '小白', '小華']
+// console.log('轉學前：', classA)
+
+classA.push('小黃')
+// console.log('轉學後：', classA)
+
+let igPhotos = [
+  'https://xx.img.com/a1.png',
+  'https://xx.img.com/a2.png',
+  'https://xx.img.com/a3.png'
+]
+
+// console.log('第1筆', igPhotos[0]) // index = 0
+// console.log('第2筆', igPhotos[1])
+// console.log('第3筆', igPhotos[2])
+// console.log('第4筆', igPhotos[4])
+
+// TODO del
+// console.log('igPhotos', igPhotos)
+// console.log('igPhotos len', igPhotos.length)
+// 長度
+// TODO del
+// console.log(classA.length)
