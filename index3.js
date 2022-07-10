07ef11cc05d1ea3d302c54f79e0bf20634388057
ALICE LIANG <81024991+Art-liang@users.noreply.github.com>
2022-07-10 19:53:15 +0800

    流程控制 if else

---
+++ index3.js
@@ -0,0 +1,52 @@
+// true or false 直接做一個if的判斷
+// let condition = 100 < 50
+// console.log('condition', condition );
+//  if (100 > 50) {
+
+
+// + - * / /算術運算子
+// &&  || ! 邏輯運算子
+
+// && 兩邊都應該是true 結果才是 true
+||
+// let condition = true && false
+// console.log ( 'condition')
+
+//     if (condition){
+
+//     }
+//     /*條件-當我們條件成立要做的事情 */
+
+//     console.log('確實大於50')
+
+//     } else{
+//         //當我們條件沒有成立要做的事情
+//         console.log('錯誤')
+//     }
+
+// let (soreA > scoreB) {
+  
+//     console.log ( 'A考的比較高分' )
+// }
+// else{
+//     console.log ( 'B考得比較高分')
+    
+// }
+
+// if的用法
+// let score = 60 /*分數可變動然後演變成下面選項的*/
+
+// if(score === 100){
+//     console.log('好棒棒')
+// }else if (score === 70){
+//     console.log('在加油')
+
+// }else if (score === 60){
+//     console.log('下次小心點')
+// } else {
+//     console.log('去補修!')
+// }
+   
+// 群組的概念寫法做成過濾表
+
+
